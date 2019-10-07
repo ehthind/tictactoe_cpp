391b3ea47c3fea9aca66ba451dd1593b6ecbc0a4
Amrit Thind <amrit@Amrits-MacBook-Pro.local>
2019-10-06 23:34:36 -0400

    Board class + overwrite ToString to print out board.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,35 @@
 
 using namespace std;
 
+class Board {
+    protected:
+        string board[9];
+    public:
+        Board();
+        void ToString() {
+            for(int i = 0; i < 9; i ++ ){
+                cout << ' ' << this -> board[i];
+                if (i != 2 && i != 5 && i != 8) {
+                    cout << ' ' << '|';
+                }
+                if (i == 2 || i == 5 || i == 8) { 
+                    cout << endl;
+                    cout << '-' << '-' << '-' << '+' << '-' << '-' << '-' << '+' << '-' << '-' << '-';
+                    cout << endl;
+                }
+            }
+        };
+};
+
+Board::Board() {
+    for(int i = 0; i < 9; i ++ ){
+        this -> board[i] = to_string(i+1);
+    }
+};
+
 int main() {
-    cout << "Program started";
+    Board ticTacToe;
+    ticTacToe.ToString();
+
+    return 0;
 }
