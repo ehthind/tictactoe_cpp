681fe7b456ff2c1592d00cb44dadeb8a9738771d
Amrit Thind <amrit@Amrits-MacBook-Pro.local>
2019-10-07 08:27:24 -0400

    Clean up row divider, move constructor in class.

--- main.cpp
+++ main.cpp
@@ -9,26 +9,23 @@ class Board {
     protected:
         string board[9];
     public:
-        Board();
+        Board() {
+            for(int i = 0; i < 9; i ++ ) {
+                this -> board[i] = to_string(i+1);
+            }
+        }
+
         void ToString() {
-            for(int i = 0; i < 9; i ++ ){
+            for(int i = 0; i < 9; i ++ ) {
                 cout << ' ' << this -> board[i];
                 if (i != 2 && i != 5 && i != 8) {
                     cout << ' ' << '|';
-                }
-                if (i == 2 || i == 5 || i == 8) { 
-                    cout << endl;
-                    cout << '-' << '-' << '-' << '+' << '-' << '-' << '-' << '+' << '-' << '-' << '-';
-                    cout << endl;
+                } else if (i == 2 || i == 5 || i == 8) { 
+                    const string rowDivider = "---+---+---";
+                    cout << endl << rowDivider << endl;
                 }
             }
         };
-};
-
-Board::Board() {
-    for(int i = 0; i < 9; i ++ ){
-        this -> board[i] = to_string(i+1);
-    }
 };
 
 int main() {
